199b6ee5c08708ccdb719e554de06205d586b675
agnewee <agnewee@gmail.com>
2018-01-06 16:29:12 +0800

    export environ

--- build.sh
+++ build.sh
@@ -1,18 +1,22 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-ATSSH_ROOT=${HOME}/.atssh
+ATSSH_ROOT=$HOME/.atssh
 
 if [ -z $1 ]; then
     echo "Please enter shell profile path"
+    echo "Just like ~/.zshrc ~/.bashrc"
     exit 1
 fi
 
-if [ ! -d ${ATSSH_ROOT} ]; then
-    mkdir ${ATSSH_ROOT}/bin
+if [ ! -d $ATSSH_ROOT ]; then
+    mkdir -p $ATSSH_ROOT/bin
 fi
 
-echo "export ATSSH_ROOT=${ATSSH_ROOT}"
-echo "export PATH=\$ATSSH_ROOT/bin:\$PATH"
+echo "export ATSSH_ROOT=$ATSSH_ROOT" >> $1
+echo "export PATH=\$ATSSH_ROOT/bin:\$PATH" >> $1
+export ATSSH_ROOT=$ATSSH_ROOT
+export PATH=$ATSSH_ROOT/bin:$PATH
 
-cp -f atssh ${ATSSH_ROOT}/bin
-cp -f atssh.py ${ATSSH_ROOT}/bin
+cp -f atssh $ATSSH_ROOT/bin
+cp -f atssh.py $ATSSH_ROOT/bin
+chmod +x $ATSSH_ROOT/bin/atssh
