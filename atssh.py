69c4e5f5ebe475d20e87da00d983c820656106d3
agnewee <agnewee@gmail.com>
2018-01-06 16:29:00 +0800

    fix bug

--- atssh.py
+++ atssh.py
@@ -4,74 +4,98 @@ import os
 import sys
 from subprocess import Popen, PIPE
 
+if sys.version_info.major == 2:
+    from ConfigParser import ConfigParser
+else:
+    from configparser import ConfigParser
+
 
 def getoutput(cmd):
     p = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True)
     out, _ = p.communicate()
-    return out.strip()
+    return out.strip(), p.wait()
 
 
 class ATSSH(object):
 
     EXPECT_STR = """set timeout 30
 spawn ssh -o "StrictHostKeyChecking no" -p{port} -l {username} {ip}
-expect "password:"
-send "{password}\r"
-interact
+expect {{
+    "assword:" {{
+        send "{password}\r";
+        exp_continue
+    }}
+    "denied" {{
+        puts "Wrong password"
+        exit 1;
+    }}
+    "ogin:" {{
+        interact
+    }}
+}}
 """
     
-    def __init__(self, ip, username=None, password=None, port=22):
-        self.ip = ip
-        self.username = username
-        self.password = password
-        self.port = port
-        self.cache_dir = None
-        self.init_home_dir()
-    
-    def init_home_dir(self):
-        home_dir = getoutput('cd ~; pwd')
-        self.cache_dir = os.path.join(home_dir, '.atssh')
-        if not os.path.exists(self.cache_dir):
-            os.mkdir(self.cache_dir)
-    
-    def query_cache_file(self):
-        expect_file = os.path.join(self.cache_dir, self.ip)
-        if not os.path.exists(expect_file):
-            if self.username and self.password:
-                self.gen_cache_file(expect_file)
-                return True
-            else:
-                return False
-        else:
-            return True
-            
-    def gen_cache_file(self, expect_file):
-        with open(expect_file, 'w') as f:
-            f.write(self.EXPECT_STR.format(ip=self.ip,
-                                           username=self.username,
-                                           password=self.password,
-                                           port=self.port))
-        getoutput('chmod +x {expect_file}'.format(expect_file=expect_file))
+    def __init__(self):
+        self.config_file = self._config_file()
+        self.config = self.get_config()
 
-    def run(self):
-        result = self.query_cache_file()
-        if result:
-            os.system('cd {cache_dir}; ./{expect_file}'
-                      .format(cache_dir=self.cache_dir,
-                              expect_file=self.ip))
+    def get_config(self):
+        config = ConfigParser()
+        config.read(self.config_file)
+        return config
+
+    def run(self, ip, username=None, password=None, port=22):
+        if self.has_cache(ip):
+            login_info = self.query_from_ip(ip)
+            cmd = self.EXPECT_STR.format(**login_info)
+            os.system("expect -c '{}'".format(cmd))
+        elif username and password:
+            cmd = self.EXPECT_STR.format(ip=ip,
+                                         username=username,
+                                         password=password,
+                                         port=port)
+            retcode = os.system("expect -c '{}'".format(cmd))
+            if retcode == 0:
+                self.record_login_info(ip, username, password, port)
+            else:
+                print('\nPlease confirm login username or password\n')
+                sys.exit(1)
         else:
-            print()
-            print('The %s has no cache and Cannot find Login'
-                  'info in the input message' % self.ip)
-            print()
+            print('\nThe %s has no cache and Cannot find Login'
+                  'info in the input message\n' % ip)
+            sys.exit(1)
+
+    def has_cache(self, ip):
+        return self.config.has_section(ip)
+
+    def query_from_ip(self, ip):
+        return {'ip': ip,
+                'username': self.config.get(ip, 'username'),
+                'password': self.config.get(ip, 'password'),
+                'port': self.config.get(ip, 'port')
+                }
+
+    def record_login_info(self, ip, username, password, port):
+        self.config.add_section(ip)
+        self.config.set(ip, 'username', username)
+        self.config.set(ip, 'password', password)
+        self.config.set(ip, 'port', port)
+        with open(self.config_file, 'w') as f:
+            self.config.write(f)
+
+    @staticmethod
+    def _config_file():
+        atssh_home = os.getenv('ATSSH_ROOT')
+        if not atssh_home:
+            raise OSError('Cannot load ATSSH_ROOT environ variable')
+        config_file = os.path.join(atssh_home, 'config.ini')
+        return config_file
 
 
 if __name__ == '__main__':
     argv_len = len(sys.argv)
     if argv_len < 2:
-        print()
-        print('Please input something')
-        print()
+        print('\nPlease input something\n')
         sys.exit(1)
     ip = sys.argv[1]
     if argv_len > 4:
@@ -86,5 +110,5 @@ if __name__ == '__main__':
         username = None
         password = None
         port = 22
-    pssh = ATSSH(ip, username, password, port)
-    pssh.run()
+    atssh = ATSSH()
+    atssh.run(ip, username, password, port)
